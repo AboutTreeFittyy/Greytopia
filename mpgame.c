febd8150c93a73898c6e448da5d13ab72176a00a
Mat <mathewboland@hotmail.com>
2020-08-03 15:28:45 -0400

    Good start
    Most functions added for final game. Much to be done obviously but this has the structure needed.

--- mpgame.c
+++ mpgame.c
@@ -2,8 +2,7 @@
 #include <allegro.h>
 #include "mappyal.h"
 #include "mpgame.h"
-
-//tile grabber
+/*tile grabber*/
 BITMAP *grabFrame(BITMAP *source, int width, int height, int startx, int starty, int columns, int frame){
     BITMAP *temp = create_bitmap(width,height);
     int x = startx + (frame % columns) * width;
@@ -11,7 +10,7 @@ BITMAP *grabFrame(BITMAP *source, int width, int height, int startx, int starty,
     blit(source,temp,x,y,0,0,width,height);
     return temp;
 }
-//Loads the sprites into their struct from their bitmap files
+/*Loads the sprites into their struct from their bitmap files*/
 void loadLevel(char* map){   
 	//load the map
 	MapLoad(map);
@@ -40,17 +39,17 @@ void loadLevel(char* map){
     player->startFrame = 0;
     player->alive = 1;
 }
-
+/*Collision function*/
 int collided(int x, int y){
     BLKSTR *blockdata;
 	blockdata = MapGetBlock(x/mapblockwidth, y/mapblockheight);
 	return blockdata->tl;
 }
-
+/*Input handling function, checks pressed key to see what action to take depending on set flags at time*/
 void getInput(){
 	//Check for quit game
 	if(key[KEY_ESC]){
-		quit = 0;
+		quit = 1;
 	}
 	if (key[KEY_RIGHT]){ 
         facing = 1; 
@@ -95,83 +94,98 @@ void getInput(){
     }
 }
 
+void updateMap(){
+	//update the map scroll position
+	mapxoff = player->x + player->width/2 - WIDTH/2 + 10;
+	mapyoff = player->y + player->height/2 - HEIGHT/2 + 10;
+    //avoid moving beyond the map edge
+	if(mapxoff < 0){
+		mapxoff = 0;
+	}
+	if(mapxoff > (mapwidth * mapblockwidth - WIDTH)){
+		mapxoff = mapwidth * mapblockwidth - WIDTH;
+	}            
+	if(mapyoff < 0){
+		mapyoff = 0;
+	}            
+	if(mapyoff > (mapheight * mapblockheight - HEIGHT)){
+		mapyoff = mapheight * mapblockheight - HEIGHT;
+	}
+	//draw the background tiles
+	MapDrawBG(buffer, mapxoff, mapyoff, 0, 0, WIDTH-1, HEIGHT-1);
+    //draw foreground tiles
+	MapDrawFG(buffer, mapxoff, mapyoff, 0, 0, WIDTH-1, HEIGHT-1, 0);
+}
+
+void updateSprites(){
+	//draw the player's sprite
+	if(facing){
+		draw_sprite(buffer, player_image[player->curframe], (player->x-mapxoff), (player->y-mapyoff+1));
+	}else{
+		draw_sprite_h_flip(buffer, player_image[player->curframe], (player->x-mapxoff), (player->y-mapyoff));
+	}
+}
+/*Loop used during title screen, handles selection/progression to gameloop or exiting application*/
 void titleLoop(){
 	
 }
-
+/*The main loop run when game in progress, handles flow of calls and checks as well as updating screen*/
 void gameLoop(){
-	
+	//oldpy = player->y; 
+    //oldpx = player->x;
+    if(keypressed()){ //Get keyboard input when key(s) pressed
+    	getInput();
+	}    
+    //check for collided with foreground tiles
+	if(!facing){ 
+        if(collided(player->x, player->y + player->height)){
+        	player->x = oldpx; 
+		}                
+    }else{ 
+        if(collided(player->x + player->width, player->y + player->height)){
+        	player->x = oldpx; 
+		}               
+    }		
+    updateMap();
+    updateSprites();
+    //blit the double buffer 
+	vsync();
+    acquire_screen();
+	blit(buffer, screen, 0, 0, 0, 0, WIDTH-1, HEIGHT-1);
+    release_screen();
 }
-
+/*Main function, handles initial loading. Passes on taks to be handled in title/game loop functions. When quit, destroys allocated memory.*/
 int main(void){
-    facing = 0, jump = JUMPIT, quit = 1;
+    facing = 0, jump = JUMPIT, quit = 0;
 	allegro_init();	
 	install_timer();
 	install_keyboard();
 	set_color_depth(16);
 	set_gfx_mode(MODE, WIDTH, HEIGHT, 0, 0);
-	loadLevel("images/test.FMP");//loads the specified level and the sprites with it
-    
+	printf(".LOADING");
+	/***This is set up this way so that multiple levels can be implemented mroe easily
+		Try having level select from title screen or next map loaded when previous finished***/	
+	loadLevel("images/test.FMP");//loads the specified level and the sprites with it  
+	printf(".LOADED");  
     //create the double buffer
 	buffer = create_bitmap(WIDTH, HEIGHT);
 	clear(buffer);
     //master loop
-	while(quit){
-		oldpy = player->y; 
-        oldpx = player->x;
-        if(keypressed()){ //Get keyboard input when key(s) pressed
-        	getInput();
-		}
-        
-        //check for collided with foreground tiles
-		if(!facing){ 
-            if(collided(player->x, player->y + player->height)){
-            	player->x = oldpx; 
-			}                
-        }else{ 
-            if(collided(player->x + player->width, player->y + player->height)){
-            	player->x = oldpx; 
-			}               
-        }		
-        //update the map scroll position
-		mapxoff = player->x + player->width/2 - WIDTH/2 + 10;
-		mapyoff = player->y + player->height/2 - HEIGHT/2 + 10;
-        //avoid moving beyond the map edge
-		if(mapxoff < 0){
-			mapxoff = 0;
-		}
-		if(mapxoff > (mapwidth * mapblockwidth - WIDTH)){
-			mapxoff = mapwidth * mapblockwidth - WIDTH;
-		}            
-		if(mapyoff < 0){
-			mapyoff = 0;
-		}            
-		if(mapyoff > (mapheight * mapblockheight - HEIGHT)){
-			mapyoff = mapheight * mapblockheight - HEIGHT;
-		}
-		//draw the background tiles
-		MapDrawBG(buffer, mapxoff, mapyoff, 0, 0, WIDTH-1, HEIGHT-1);
-        //draw foreground tiles
-		MapDrawFG(buffer, mapxoff, mapyoff, 0, 0, WIDTH-1, HEIGHT-1, 0);
-        //draw the player's sprite
-		if(facing){
-			draw_sprite(buffer, player_image[player->curframe], (player->x-mapxoff), (player->y-mapyoff+1));
-		}else{
-			draw_sprite_h_flip(buffer, player_image[player->curframe], (player->x-mapxoff), (player->y-mapyoff));
-		}
-        //blit the double buffer 
-		vsync();
-        acquire_screen();
-		blit(buffer, screen, 0, 0, 0, 0, WIDTH-1, HEIGHT-1);
-        release_screen();
+	while(!quit){		
+		//Sudo code for when implementing title screen		
+		//Have gameLoop run while a new flag "playing" is = 1
+		//Have if else in getInput for ESC so if "playing" then only that is turned off to return to title, otherwise "quit" exiting application		
+		gameLoop();//Plays the game
 	} //while
-
+	//Cleanup
+	printf(".CLEANING");
     for (n=0; n<8; n++){
     	destroy_bitmap(player_image[n]);
 	}        
     free(player);
 	destroy_bitmap(buffer);
 	MapFreeMem ();
+	printf(".CLEANED");
 	allegro_exit();
 	return 0;
 }
